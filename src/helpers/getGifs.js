d9dac8f89c68f23fd508782fe253cddbe7ba8ef0
muriel gonzalez <muriel.glez@gmail.com>
2022-07-06 10:26:48 +0200

    update pro

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=rfBqO8Jb6XIfwU6FfWEbbrSB8LiGsvAX&q=${category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=cV9riYP8se6ic3OCFUOvxAMPVmiWlEyM&q=${category}&limit=10`;
     const resp = await fetch( url );
     const {data} = await resp.json();
     
